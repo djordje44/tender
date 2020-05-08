969729c333b47437ffdf14be94e81a3fddeec15a
Djordje Slavkovic <slavkovic.djordje@gmail.com>
2020-05-08 10:42:57 +0200

    Initial commit

---
+++ src/test/java/ch/olmero/tender/TenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.olmero.tender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
